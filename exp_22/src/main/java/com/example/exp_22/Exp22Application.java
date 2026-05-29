cc7bec4d7d38ec8fc0b71878b8fe14dc99d8270d
Zakofr777 <zaurilabadze@gmail.com>
2026-05-29 15:27:58 +0400

    init

---
+++ exp_22/src/main/java/com/example/exp_22/Exp22Application.java
@@ -0,0 +1,13 @@
+package com.example.exp_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exp22Application.class, args);
+    }
+
+}
