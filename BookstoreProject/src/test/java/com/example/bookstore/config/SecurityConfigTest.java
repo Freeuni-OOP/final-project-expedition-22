be416150b783a3a97111d79817d8dbcaafb87ec4
Zakofr777 <zaurilabadze@gmail.com>
2026-06-28 16:49:52 +0400

    tests added, logout yet to be implemented

--- BookstoreProject/src/test/java/com/example/bookstore/config/SecurityConfigTest.java
+++ BookstoreProject/src/test/java/com/example/bookstore/config/SecurityConfigTest.java
@@ -38,4 +38,5 @@ class SecurityConfigTest {
     void securedEndpoint_ShouldAllowAuthenticatedUser() throws Exception {
         mockMvc.perform(get("/any-other-protected-endpoint")).andExpect(status().isNotFound());
     }
+
 }
